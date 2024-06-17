8e7e24b4cf5bf5312e5e0b02f0213de428c4030b
Luis-Mackiewicz <luismiguelpix@gmail.com>
2024-06-17 11:19:16 -0300

    introduzindo php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ArcadeHub</title>
+</head>
+<body>
+    <header class="header">
+        <a href="home" title="Home" class="header-logo">
+            <img src="./Imagens/ArcadeHub.png" alt="ArcadeHub">
+        </a>
+
+        <a href="" title="Mostrar Menu" class="header-menu">
+        <img src="./Imagens/Menu.png">
+        </a>
+
+    </header>
+    <main>
+
+        <?php
+if (
+    isset($_GET["param"])
+    ) {
+        $param = $_GET["param"];
+    }
+    
+    $p= explode("/", $param);
+    
+    
+    echo$page = $p[0] ?? "home";
+    $jogo = $p[1] ?? NULL;
+    
+    if ($page == "jogo") {
+        echo $pagina = "jogo/{$jogo}.php";
+    }
+    
+    else {
+        echo $pagina = "paginas/{$page}.php";
+    }
+    
+    
+    
+    if (file_exists($pagina)) {
+        include $pagina;
+        
+    }
+    
+    else {
+        include "./erro.php";
+    } 
+    ?>
+    </main>
+    <footer>
+        <p>Desenvolvido Por Luis Miguel Mackiewicz</p>
+    </footer>
+    </body>
+    </html>
---
+++ paginas/contato.php
@@ -0,0 +1,3 @@
+<div 
+class= "Entre em contato
+</div>
