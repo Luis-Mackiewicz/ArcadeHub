00e8b532f1b93a7c336bc5713944c7f4c1ca88b3
Luis-Mackiewicz <luismiguelpix@gmail.com>
2024-06-17 11:59:56 -0300

    mudanças na Navbar

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ArcadeHub</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="shortcut icon" href="./Imagens/ArcadeHub.png">
 </head>
 <body>
     <header class="header">
         <a href="home" title="Home" class="header-logo">
-            <img src="./Imagens/ArcadeHub.png" alt="ArcadeHub">
+            <img src="./Imagens/LogoMarca.png" alt="ArcadeHub">
         </a>
 
         <a href="" title="Mostrar Menu" class="header-menu">
