265584dfb9db37af01c5d069409de00ecb2362a4
Luis-Mackiewicz <luismiguelpix@gmail.com>
2024-06-20 21:41:17 -0300

    site atualizado com php e bs

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer id="footer">
+    <ul class="social-icons">
+        <li><a href="https://www.instagram.com/luis.mackiewicz/" target="_blank"><img src="./imagens/imgFooter/insta.png" alt="Instagram"></a></li>
+        <li><a href="https://github.com/Luis-Mackiewicz" target="_blank"><img src="./imagens/imgFooter/Github.png" alt="Github"></a></li>
+        <li><a href="mailto:luismiguelpix@gmail.com"><img src="./imagens/imgFooter/email.png" alt="E-mail"></a></li>
+    </ul>
+</footer>
+
---
+++ paginas/home.php
@@ -0,0 +1,58 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="../imagens/BambooStrike.png" class="card-img-top" alt="BombSword">
+                <div class="card-body">
+                    <p class="card-text">Bomb Sword</p>
+                    <a href="index.php?param=jogo1" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="imagens/jogo2.jpg" class="card-img-top" alt="Jogo 2">
+                <div class="card-body">
+                    <p class="card-text">Jogo 2</p>
+                    <a href="index.php?param=jogo2" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="imagens/jogo3.jpg" class="card-img-top" alt="Jogo 3">
+                <div class="card-body">
+                    <p class="card-text">Jogo 3</p>
+                    <a href="index.php?param=jogo3" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="imagens/jogo4.jpg" class="card-img-top" alt="Jogo 4">
+                <div class="card-body">
+                    <p class="card-text">Jogo 4</p>
+                    <a href="index.php?param=jogo4" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="imagens/jogo5.jpg" class="card-img-top" alt="Jogo 5">
+                <div class="card-body">
+                    <p class="card-text">Jogo 5</p>
+                    <a href="index.php?param=jogo5" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="imagens/jogo6.jpg" class="card-img-top" alt="Jogo 6">
+                <div class="card-body">
+                    <p class="card-text">Jogo 6</p>
+                    <a href="index.php?param=jogo6" class="btn btn-primary">Saiba mais</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
