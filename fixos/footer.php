8ff175359586d062f168c25e2a745d1b83db3fb2
Luis-Mackiewicz <luismiguelpix@gmail.com>
2024-07-02 16:46:00 -0300

    correção do scss

--- fixos/footer.php
+++ fixos/footer.php
@@ -1,32 +1,32 @@
-<!-- Footer -->
-<footer class="footer">
-    <div class="container">
-        <div class="row align-items-center justify-content-center">
-            <!-- Icons -->
-            <div class="col-auto">
-                <a href="https://github.com/Luis-Mackiewicz/ArcadeHub" target="_blank">
-                    <img src="../assets/imagens/git.png" alt="GitHub" class="footer-icon">
-                </a>
-                <a href="https://www.instagram.com/luis.mackiewicz/" target="_blank">
-                    <img src="../assets/imagens/instagram.png" alt="Instagram" class="footer-icon">
-                </a>
-                <a href="mailto:luismiguelpix@gmail.com">
-                    <img src="../assets/imagens/gmail.png" alt="Gmail" class="footer-icon">
-                </a>
+        <!-- Footer -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row align-items-center justify-content-center">
+                    <!-- Icons -->
+                    <div class="col-auto">
+                        <a href="https://github.com/Luis-Mackiewicz/ArcadeHub" target="_blank">
+                            <img src="../assets/imagens/git.png" alt="GitHub" class="footer-icon">
+                        </a>
+                        <a href="https://www.instagram.com/luis.mackiewicz/" target="_blank">
+                            <img src="../assets/imagens/instagram.png" alt="Instagram" class="footer-icon">
+                        </a>
+                        <a href="mailto:luismiguelpix@gmail.com">
+                            <img src="../assets/imagens/gmail.png" alt="Gmail" class="footer-icon">
+                        </a>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col text-center">
+                        <p class="footer-text">Equipe 4 - Todos os direitos reservados</p>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="row mt-3">
-            <div class="col text-center">
-                <p class="footer-text">Equipe 4 - Todos os direitos reservados</p>
-            </div>
-        </div>
-    </div>
-</footer>
+        </footer>
 
-<!-- Scripts -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+5hb7n4n5t6z5b3c5a2P7h5m4Er4Qp4h6I6g8b9" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.7/umd/popper.min.js" integrity="sha384-IQsoLXl+PILFyfxnBdL6RIRx0W5OUoFi8rP4M1pQsmf1m6auXMOJp7tr4l9c6KN3" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></script>
-<script src="../assets/js/scripts.js"></script>
-</body>
+        <!-- Scripts -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+5hb7n4n5t6z5b3c5a2P7h5m4Er4Qp4h6I6g8b9" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.7/umd/popper.min.js" integrity="sha384-IQsoLXl+PILFyfxnBdL6RIRx0W5OUoFi8rP4M1pQsmf1m6auXMOJp7tr4l9c6KN3" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></script>
+        <script src="../assets/js/scripts.js"></script>
+    </body>
 </html>
--- fixos/header.php
+++ fixos/header.php
@@ -7,77 +7,51 @@
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Estilos customizados -->
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
     <!-- Font Awesome para ícones -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="icon" href="../assets/imagens/arcade_hub.png" type="image/png">
-
+    <link rel="shortcut icon" href="../assets/imagens/arcade_hub.png">
 </head>
 <body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <!-- Logo -->
-            <a class="navbar-brand" href="../paginas/home.php">
-                <img src="../imagens/arcade_hub.png" alt="Logo" height="30">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="../paginas/home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../paginas/equipe.php">Equipe</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Games
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="../paginas/bamboo_strike.php">Bamboo Strike</a></li>
-                            <li><a class="dropdown-item" href="../paginas/bomb_sword.php">Bomb Sword</a></li>
-                            <li><a class="dropdown-item" href="../paginas/bubble_kid.php">Bubble Kid</a></li>
-                            <li><a class="dropdown-item" href="../paginas/explosion_race.php">Explosion Race</a></li>
-                            <li><a class="dropdown-item" href="../paginas/desordeon.php">Desordeon</a></li>
-                            <li><a class="dropdown-item" href="../paginas/speed_bird.php">Speed Bird</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">
-                            <i class="fas fa-sign-in-alt"></i> Login
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <!-- Modal de Login -->
-    <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="loginModalLabel">Login</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="../auth/atenticacao.php" method="POST">
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Senha</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Entrar</button>
-                    </form>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="../index.php">
+                    <img src="../assets/imagens/arcade_hub.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                    ArcadeHub
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="../paginas/home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../paginas/equipe.php">Equipe</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownGames" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Jogos
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownGames">
+                                <?php if (isset($games) && is_array($games)) : ?>
+                                    <?php foreach ($games as $game) : ?>
+                                        <li><a class="dropdown-item" href="../paginas/<?php echo $game['slug']; ?>.php"><?php echo $game['title']; ?></a></li>
+                                    <?php endforeach; ?>
+                                <?php else : ?>
+                                    <li><a class="dropdown-item" href="#">Nenhum jogo disponível</a></li>
+                                <?php endif; ?>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </div>
-    </div>
+        </nav>
+    </header>
+    <main>
+        <!-- Conteúdo das páginas será inserido aqui -->
